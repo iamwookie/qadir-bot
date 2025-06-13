2cd55322d8a92f8004a606db9fb8bd0663ccc567
Bilal <bq1214@gmail.com>
2025-06-13 12:50:37 +0500

    feat: update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 
 
 class QadirBot(discord.Bot):
-    def __init__(self, description=None, *args, **options):
-        super().__init__(description, *args, **options)
+    def __init__(self, *args, **options):
+        super().__init__(*args, **options)
 
     async def on_ready(self):
         print(f"Logged in as {self.user}")
