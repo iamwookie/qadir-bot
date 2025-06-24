d74f2acfb6002903496ff7a67c44edcbfdeb542a
Bilal <bq1214@gmail.com>
2025-06-24 12:17:20 +0500

    fix: proposal processing

--- cogs/proposals.py
+++ cogs/proposals.py
@@ -47,24 +47,17 @@ class ProposalsCog(commands.Cog, guild_ids=GUILD_IDS):
         except Exception:
             logger.exception("[COG] ProposalsCog Handler Error:")
 
-    @commands.slash_command()
-    @commands.has_any_role(*ROLE_IDS)
-    async def propose(self, ctx: discord.ApplicationContext) -> None:
-        """Submit a proposal."""
-
-        modal = CreateProposalModal(title="Create a Proposal")
-        await ctx.send_modal(modal)
-
     @tasks.loop(hours=24)
     async def process_proposals(self) -> None:
         """Process active proposals."""
 
-        logger.info("[TASK] Running Proposals Processing...")
+        logger.info("⌛ Running proposals processing...")
 
         proposals = await self.bot.redis.smembers("qadir:proposals")
         proposals = [json.loads(data) for data in proposals]
 
         if not proposals:
+            logger.info("⌛ No proposals to process.")
             return
 
         for data in proposals:
@@ -75,12 +68,12 @@ class ProposalsCog(commands.Cog, guild_ids=GUILD_IDS):
                 if (datetime.now(timezone.utc) - message.created_at).total_seconds() < 86400:
                     continue
 
-                upvotes = sum(1 for reaction in message.reactions if reaction.emoji == "👍") - 1
-                downvotes = sum(1 for reaction in message.reactions if reaction.emoji == "👎") - 1
+                upvotes = sum(reaction.count for reaction in message.reactions if reaction.emoji == "👍") - 1
+                downvotes = sum(reaction.count for reaction in message.reactions if reaction.emoji == "👎") - 1
 
-                embed = discord.Embed(description="Voting has ended for this proposal.", colour=0xFF0000)
-                embed.add_field(name="Upvotes", value=str(upvotes), inline=True)
-                embed.add_field(name="Downvotes", value=str(downvotes), inline=True)
+                embed = discord.Embed(title="Proposal Closed", description="Voting has ended for this proposal.", colour=0xFF0000)
+                embed.add_field(name="Upvotes", value=f"`{str(upvotes)}`", inline=True)
+                embed.add_field(name="Downvotes", value=f"`{str(downvotes)}`", inline=True)
 
                 await thread.send(embed=embed)
                 await thread.edit(locked=True)
@@ -92,7 +85,7 @@ class ProposalsCog(commands.Cog, guild_ids=GUILD_IDS):
             except Exception:
                 logger.exception(f"[TASK] Error Processing Proposal {data['thread_id']}:")
 
-        logger.info(f"[TASK] Proccessed {len(proposals)} Proposals.")
+        logger.info(f"⌛ Proccessed {len(proposals)} proposals.")
 
     @process_proposals.before_loop
     async def before_process_proposals(self) -> None:
@@ -101,6 +94,14 @@ class ProposalsCog(commands.Cog, guild_ids=GUILD_IDS):
     @process_proposals.error
     async def process_proposals_error(self, error: Exception) -> None:
         logger.error("[TASK] Proposals Processing Error:", exc_info=error)
+
+    @commands.slash_command()
+    @commands.has_any_role(*ROLE_IDS)
+    async def propose(self, ctx: discord.ApplicationContext) -> None:
+        """Submit a proposal."""
+
+        modal = CreateProposalModal(title="Create a Proposal")
+        await ctx.send_modal(modal)
 
 
 def setup(bot: QadirBot):
