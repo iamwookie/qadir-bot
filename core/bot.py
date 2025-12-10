331b0819be22cf657aa6b31e3946c7a17e80b34b
Bilal <mail@bil.al>
2025-12-10 12:11:40 +0000

    style: remove version from status

--- core/bot.py
+++ core/bot.py
@@ -50,7 +50,7 @@ class Qadir(discord.Bot):
         for cog in self.cogs:
             logger.info(f"🔗 Loaded Cog: {cog}")
 
-        await self.change_presence(activity=discord.CustomActivity(name=f"v{config['app']['version']} • /help"))
+        await self.change_presence(activity=discord.CustomActivity(name="/help"))
 
         if not self._initialised.is_set():
             self._initialised.set()
